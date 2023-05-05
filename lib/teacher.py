7b8791bf6c5bf7629b7645cc240976f34f631874
Dustin <dustinhollowaypdx@gmail.com>
2023-05-05 16:04:45 -0700

    committed

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,18 @@ from user import User
 import random
 
 class Teacher(User):
+    knowledge = ["str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",]
+
+    def __init_(self, knowledge):
+        self.knowledge = knowledge
+
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
